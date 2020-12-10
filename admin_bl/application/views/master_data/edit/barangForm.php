845155fb782291c4ff81a3033a751b714cf9aa6a
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2020-12-10 21:02:14 +0700

    fix barang

--- admin_bl/application/views/master_data/edit/barangForm.php
+++ admin_bl/application/views/master_data/edit/barangForm.php
@@ -1,138 +1,138 @@
 <!-- <style>
 
-  </style> -->
-  <div class="container-fluid" id="container-wrapper">
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Data Barang</h1>
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?= site_url('dashboard') ?>">Home</a></li>
-        <li class="breadcrumb-item">Data Master</li>
-        <li class="breadcrumb-item"><a href="<?= site_url('Master_Data/barang') ?>">Barang</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Edit Data</li>
-      </ol>
-    </div>
+</style> -->
+<div class="container-fluid" id="container-wrapper">
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Data Barang</h1>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="<?= site_url('dashboard') ?>">Home</a></li>
+      <li class="breadcrumb-item">Data Master</li>
+      <li class="breadcrumb-item"><a href="<?= site_url('Master_Data/barang') ?>">Barang</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Edit Data</li>
+    </ol>
+  </div>
 
-    <!-- Row -->
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card mb-4">
-          <div class="col-12">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-default">Edit Data Barang</h6>
-              <a href="<?= site_url('Master_Data/barang') ?>" type="button" class="btn btn-info"><i class="fas fa-arrow-left"> Back</i></a>
-            </div>
-            <div class="card-body">
-              <!-- Alert jika gagal insert -->
-              <?php if ($this->session->flashdata('insertBarang') == 'failed'): ?>
+  <!-- Row -->
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="card mb-4">
+        <div class="col-12">
+          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-default">Edit Data Barang</h6>
+            <a href="<?= site_url('Master_Data/barang') ?>" type="button" class="btn btn-info"><i class="fas fa-arrow-left"> Back</i></a>
+          </div>
+          <div class="card-body">
+            <!-- Alert jika gagal insert -->
+            <?php if ($this->session->flashdata('insertBarang') == 'failed'): ?>
+              <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>		
+                <span>Gagal Memasukan Data</span>
+              </div>
+            <?php endif; ?>
+
+            <?php if ($this->session->flashdata('insertBarang') == 'berhasil'): ?>
+              <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>		
+                <span>Berhasil Memasukan Data</span>
+              </div>
+            <?php endif; ?>
+
+            <?php if (($this->session->flashdata('insertBarang') != 'failed') && ($this->session->flashdata('insertBarang') != 'berhasil') && $this->session->flashdata('insertBarang') != NULL):
+            $error = $this->session->flashdata('insertBarang');
+            for ($i=0; $i < count($error); $i++) { 
+
+              if ($error[$i]=='nama') {
+                ?>
                 <div class="alert alert-danger alert-dismissible" role="alert">
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>		
-                  <span>Gagal Memasukan Data</span>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>    
+                  <span>Nama Barang Sudah Ada</span>
                 </div>
-              <?php endif; ?>
-
-              <?php if ($this->session->flashdata('insertBarang') == 'berhasil'): ?>
-                <div class="alert alert-success alert-dismissible" role="alert">
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>		
-                  <span>Berhasil Memasukan Data</span>
-                </div>
-              <?php endif; ?>
-
-              <?php if (($this->session->flashdata('insertBarang') != 'failed') && ($this->session->flashdata('insertBarang') != 'berhasil') && $this->session->flashdata('insertBarang') != NULL):
-              $error = $this->session->flashdata('insertBarang');
-              for ($i=0; $i < count($error); $i++) { 
-
-                if ($error[$i]=='nama') {
-                  ?>
-                  <div class="alert alert-danger alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>    
-                    <span>Nama Barang Sudah Ada</span>
-                  </div>
-                  <?php
-                } 
-              } endif;  ?>
-              <!--  -->
-              <!-- <form enctype="multipart/form-data" method="POST" action="<?php // site_url('Master_Data/addBarang') ?>"> -->
+                <?php
+              } 
+            } endif;  ?>
+            <!--  -->
+            <!-- <form enctype="multipart/form-data" method="POST" action="<?php // site_url('Master_Data/addBarang') ?>"> -->
               <?= form_open_multipart('Master_Data/editBarang') ?>
-                <div class="form-group">
-                  <input type="hidden" name="idbarang" value="<?= $detail->barang_id ?>">
-                  <label for="nama">Nama Barang</label>
-                  <input type="text" class="form-control" name='nama' id="nama" aria-describedby="nama"
-                  placeholder="Nama Barang" maxlength="60" value="<?= isset($detail->nama)? $detail->nama :'' ?>" required autofocus>
-                </div>
-
-                <div class="form-group">
-                  <label for="harga">Harga Barang</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text" id="label-harga">Rp. </span>
-                    </div>
-                    <input type="number" class="form-control" name='harga' id="harga" aria-describedby="Harga Barang"
-                    placeholder="Harga Barang" maxlength="25" aria-describedby="label-harga" value="<?= isset($detail->harga)? $detail->harga :'' ?>" required>
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <label for="link">Link Shopee</label>
-                  <input type="url" class="form-control" name='link' id="link" aria-describedby="link"
-                  placeholder="Link Shopee" value="<?= isset($detail->link)? $detail->link :'' ?>">
-                </div>
-                
-                <div class="form-group">
-                  <label for="kategoriop">Kategori</label>
-                  <select id="kategoriop" name="kategoriop" class="form-control selectpicker" data-live-search="true" data-size="4" required>
-                    <option value="<?php echo isset($detail->kategori_id)? $detail->kategori_id :'' ?>" selected><?php echo isset($detail->kategori_id)? $detail->KategoriName :'' ?></option>
-                    <?php foreach ($listKategori as $lkat): ?>
-                      <option value="<?= $lkat->kategori_id ?>"><?= $lkat->nama ?></option>
-                    <?php endforeach; ?>
-                  </select>
-                  <input type="hidden" id="kategorihidden" value="<?= $detail->kategori_id ?>" >
-                </div>
-
-                <div class="form-group ukuranListbox">
-                  <label for="ukuranop">Ukuran</label>
-                  <div class="input-group">
-                   <select id="ukuranop" class="form-control selectpicker" data-live-search="true" data-size="4" >
-                    <option value="" disabled>-- Pilih Ukuran --</option>
-                    <?php foreach ($listUkuran as $lk): ?>
-                      <option value="<?= $lk->ukuran_id ?>"><?= $lk->nama.' ('.$lk->singkatan.')' ?></option>
-                    <?php endforeach; ?>
-                  </select>
-                  <div class="input-group-append">
-                    <span class="input-group-text" style="cursor: pointer;"><a class="addUkuranBtn">Tambah</a></span>
-                  </div>
-                </div>
-                <div id="daftarUkuran" class="daftarUkuran">
-                      <!--  -->
-                </div>
-                <input type="hidden" name="hiddenUkuran" id="hiddenUkuran" value="<?= $detail->ukuran_id; ?>">
+              <div class="form-group">
+                <input type="hidden" name="idbarang" value="<?= $detail->barang_id ?>">
+                <label for="nama">Nama Barang</label>
+                <input type="text" class="form-control" name='nama' id="nama" aria-describedby="nama"
+                placeholder="Nama Barang" maxlength="60" value="<?= isset($detail->nama)? $detail->nama :'' ?>" required autofocus>
               </div>
 
               <div class="form-group">
-                <label for="warnaop">Warna</label>
+                <label for="harga">Harga Barang</label>
                 <div class="input-group">
-                  <select id="warnaop" class="form-control selectpicker" data-live-search="true" data-size="4">
-                    <option value="" disabled selected>-- Pilih Warna --</option>
-                    <?php foreach ($listWarna as $lw): ?>
-                      <option value="<?= $lw->warna_id ?>"><?= $lw->nama ?></option>
-                    <?php endforeach; ?>
-                  </select>
-                  <div class="input-group-append">
-                    <span class="input-group-text" style="cursor: pointer;"><a class="addWarnaBtn">Tambah</a></span>
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="label-harga">Rp. </span>
                   </div>
+                  <input type="number" class="form-control" name='harga' id="harga" aria-describedby="Harga Barang"
+                  placeholder="Harga Barang" maxlength="25" aria-describedby="label-harga" value="<?= isset($detail->harga)? $detail->harga :'' ?>" required>
                 </div>
-                <div id="daftarWarna" class="daftarUkuran"></div>
-                <input type="hidden" name="hiddenWarna" id="hiddenWarna" value="<?= $detail->warna_id; ?>">
               </div>
 
               <div class="form-group">
-                <label for="deskripsi">Deskripsi</label>
-                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="5" placeholder="Deskripsi Barang"><?= isset($detail->link)? $detail->deskripsi :'' ?></textarea>
+                <label for="link">Link Shopee</label>
+                <input type="url" class="form-control" name='link' id="link" aria-describedby="link"
+                placeholder="Link Shopee" value="<?= isset($detail->link)? $detail->link :'' ?>">
               </div>
 
-              <label for="imagesUtama">Gambar Utama: </label>
-              <img src="<?php echo base_url('assets/img/barang/').$detail->foto_utama?>" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" width="250px",height="250px" style="margin:5px;">
+              <div class="form-group">
+                <label for="kategoriop">Kategori</label>
+                <select id="kategoriop" name="kategoriop" class="form-control selectpicker" data-live-search="true" data-size="4" required>
+                  <option value="<?php echo isset($detail->kategori_id)? $detail->kategori_id :'' ?>" selected><?php echo isset($detail->kategori_id)? $detail->KategoriName :'' ?></option>
+                  <?php foreach ($listKategori as $lkat): ?>
+                    <option value="<?= $lkat->kategori_id ?>"><?= $lkat->nama ?></option>
+                  <?php endforeach; ?>
+                </select>
+                <input type="hidden" id="kategorihidden" value="<?= $detail->kategori_id ?>" >
+              </div>
+
+              <div class="form-group ukuranListbox">
+                <label for="ukuranop">Ukuran</label>
+                <div class="input-group">
+                 <select id="ukuranop" class="form-control selectpicker" data-live-search="true" data-size="4" >
+                  <option value="" disabled>-- Pilih Ukuran --</option>
+                  <?php foreach ($listUkuran as $lk): ?>
+                    <option value="<?= $lk->ukuran_id ?>"><?= $lk->nama.' ('.$lk->singkatan.')' ?></option>
+                  <?php endforeach; ?>
+                </select>
+                <div class="input-group-append">
+                  <span class="input-group-text" style="cursor: pointer;"><a class="addUkuranBtn">Tambah</a></span>
+                </div>
+              </div>
+              <div id="daftarUkuran" class="daftarUkuran">
+                <!--  -->
+              </div>
+              <input type="hidden" name="hiddenUkuran" id="hiddenUkuran" value="<?= $detail->ukuran_id; ?>">
+            </div>
+
+            <div class="form-group">
+              <label for="warnaop">Warna</label>
+              <div class="input-group">
+                <select id="warnaop" class="form-control selectpicker" data-live-search="true" data-size="4">
+                  <option value="" disabled selected>-- Pilih Warna --</option>
+                  <?php foreach ($listWarna as $lw): ?>
+                    <option value="<?= $lw->warna_id ?>"><?= $lw->nama ?></option>
+                  <?php endforeach; ?>
+                </select>
+                <div class="input-group-append">
+                  <span class="input-group-text" style="cursor: pointer;"><a class="addWarnaBtn">Tambah</a></span>
+                </div>
+              </div>
+              <div id="daftarWarna" class="daftarUkuran"></div>
+              <input type="hidden" name="hiddenWarna" id="hiddenWarna" value="<?= $detail->warna_id; ?>">
+            </div>
+
+            <div class="form-group">
+              <label for="deskripsi">Deskripsi</label>
+              <textarea class="form-control" id="deskripsi" name="deskripsi" rows="5" placeholder="Deskripsi Barang"><?= isset($detail->link)? $detail->deskripsi :'' ?></textarea>
+            </div>
+
+            <label for="imagesUtama">Gambar Utama: </label>
+            <img src="<?php echo base_url('assets/img/barang/').$detail->foto_utama?>" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" width="250px",height="250px" style="margin:5px;">
             <div class="box">
-                    <input type="hidden" name="hidden_utama" value="<?= $detail->foto_utama?>">
+              <input type="hidden" name="hidden_utama" value="<?= $detail->foto_utama?>">
               <input type="file" name="imagesUtama" id="imagesUtama" class="inputfile" />
               <label for="file-6"><figure><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg></figure> <span></span></label>
             </div>
@@ -193,18 +193,18 @@
 <!--Row-->
 </div>
 <script>
+  var  urlaj = '/bigjill/admin_bl/index.php/Master_Data/ajaxgetukuran';
   $(document).ready(function () {
-    var  urlaj = '/bigjill/admin_bl/index.php/Master_Data/ajaxgetukuran';
     var hasil = $( "#kategorihidden").val();
         // console.log(hasil);
 
-    $.ajax({
-      url: urlaj,
-      type: "POST",
-      data: {
-        validasiukuran: hasil,
-      },
-      success: function (res) {
+        $.ajax({
+          url: urlaj,
+          type: "POST",
+          data: {
+            validasiukuran: hasil,
+          },
+          success: function (res) {
         // console.log(res);
         var obj = JSON.parse(res);
         if (obj[0]['ukuran']=='Y') {
@@ -223,27 +223,27 @@
       }
     });
 
-    
-    getWarnaData();
-  });
+
+        getWarnaData();
+      });
 
   var ukurannya = [];
   var warnanya = [];
 
   function delete_ukuran_edit(size){
-      const index = ukurannya.indexOf(size);
-			if (index > -1) {
-				ukurannya.splice(index, 1);
-				$("#hiddenUkuran").val(ukurannya);
-			}
+    const index = ukurannya.indexOf(size);
+    if (index > -1) {
+      ukurannya.splice(index, 1);
+      $("#hiddenUkuran").val(ukurannya);
+    }
   }
 
   function delete_warna_edit(warna){
-      const index = warnanya.indexOf(warna);
-			if (index > -1) {
-        warnanya.splice(index, 1);
-				$("#hiddenWarna").val(warnanya);
-			}
+    const index = warnanya.indexOf(warna);
+    if (index > -1) {
+      warnanya.splice(index, 1);
+      $("#hiddenWarna").val(warnanya);
+    }
   }
 
   function getUkuranData(){
@@ -315,8 +315,7 @@
       });
     }
   }
-</script>
-<script>
+
   $('#kategoriop').on('change', function() {
     var hasil = this.value;
     // console.log(hasil);
